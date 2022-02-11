3a20c769c41c4b7f5a61192621016f5e99db3900
KhalifaGad <kkhalifa.gad@gmail.com>
2022-02-11 12:12:01 +0200

    feat: enhance hash service to be more url friendly

--- src/domain/services/Hash/hash.service.ts
+++ src/domain/services/Hash/hash.service.ts
@@ -2,9 +2,9 @@ import { randomBytes, randomUUID } from "crypto";
 
 class HashService {
   generateHash() {
-    const hexString = randomBytes(2).toString("hex").replaceAll("-", "");
+    const hexString = randomBytes(2).toString("hex");
     const uuid = randomUUID();
-    const base64 = Buffer.from(hexString, "hex");
+    const base64 = Buffer.from(hexString, "ascii");
     const randomNumber = Math.random() * 13;
     return uuid.substring(randomNumber, randomNumber + 3) + base64;
   }
