c733fcddca83fe0de17f93aa3aef9815c0ee4443
KhalifaGad <kkhalifa.gad@gmail.com>
2022-02-11 18:56:46 +0200

    feat: fire urlVisited method before redirecting

--- src/interfaces/api/controllers/Mapping/mapping.controller.ts
+++ src/interfaces/api/controllers/Mapping/mapping.controller.ts
@@ -1,18 +1,28 @@
-import { IURLService, URLService } from "@services";
+import {
+  IURLService,
+  URLService,
+  IStatisticsService,
+  StatisticsService,
+} from "@services";
 import { Request, Response } from "express";
 
 class MappingController {
-  constructor(private readonly service: IURLService) {
+  constructor(
+    private readonly urlService: IURLService,
+    private readonly statisticsService: IStatisticsService
+  ) {
     this.map = this.map.bind(this);
   }
 
   map(req: Request, res: Response) {
-    const { data } = this.service.decode(req.params.hash);
+    const hash = req.params.hash;
+    const { data } = this.urlService.decode(req.params.hash);
     if (!data) {
       return res.status(404).send({ error: "Not Found!" });
     }
+    this.statisticsService.urlVisited(hash, req.ip);
     return res.redirect(data.url);
   }
 }
 
-export default new MappingController(URLService);
+export default new MappingController(URLService, StatisticsService);
