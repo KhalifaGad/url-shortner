6b7795e249a905d7084da1a8003043dd9006af1f
KhalifaGad <kkhalifa.gad@gmail.com>
2022-02-11 06:40:44 +0200

    feat: implement add method to URLRepo

--- src/infra/db/repositories/URL/url.repo.ts
+++ src/infra/db/repositories/URL/url.repo.ts
@@ -1,19 +1,38 @@
-import { URLEntity } from "@domain";
+import { IURLEntity, URLEntity } from "@domain";
 import { ILayersContract } from "@types";
 import { URLData } from "../../data";
+import { DuplicateKeyException } from "../../exceptions";
+import { BaseException } from "../../exceptions/baseException";
 import { IRepo } from "../types";
 
-class URLRepo implements IRepo<URLEntity> {
-  add(url: URLEntity): ILayersContract<URLEntity> {
-    if (this.exist(url.hash)) {
-      return { error: new Error("SOME ERROR") };
-    }
+class URLRepo implements IRepo<IURLEntity> {
+  add(url: IURLEntity): ILayersContract<IURLEntity> {
+    const error = this.validate(url);
+    if (error) return { error };
     URLData.set(url.hash, url);
     return { data: url };
   }
-  get: (id: string) => ILayersContract<URLEntity>;
-  exist(hash: string): ILayersContract<URLEntity, boolean> {
-    return { data: URLData.has(hash) };
+
+  get: (id: string) => ILayersContract<IURLEntity, undefined>;
+
+  private validate(entity: IURLEntity): BaseException | undefined {
+    if (!this.isUniqueHash(entity.url))
+      return new DuplicateKeyException("hash already exist", {
+        isInternal: true,
+        context: `${entity.hash} has been duplicated!`,
+      });
+    if (this.isExist(entity))
+      return new DuplicateKeyException("url already exist");
+  }
+
+  private isExist(entity: IURLEntity) {
+    const rawURL = URLData.get(entity.hash);
+    const existedEntity = new URLEntity({ ...rawURL });
+    return entity.equals(existedEntity);
+  }
+
+  private isUniqueHash(hash: string) {
+    return !URLData.has(hash);
   }
 }
 
--- src/infra/db/repositories/types.ts
+++ src/infra/db/repositories/types.ts
@@ -3,5 +3,4 @@ import { ILayersContract } from "@types";
 export interface IRepo<Entity> {
   add: (data: Entity) => ILayersContract<Entity>;
   get: (id: string) => ILayersContract<Entity>;
-  exist: (id: string) => ILayersContract<Entity, boolean>;
 }
